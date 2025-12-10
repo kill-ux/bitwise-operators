00d88c05b92c7c43e22d32e7ae23e2435c0d147b
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-12-10 16:01:47 +0100

    added set and fixed read to not return result

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,33 @@ impl BitWise {
         self.value &= mask;
     }
   
-    pub fn read(self,index: Index) -> Result<bool, String> {
-        Ok(self.value >> index.get() & 1 == 1)
+    pub fn read(self,index: Index) -> bool {
+        self.value >> index.get() & 1 == 1
+    }
+
+    pub fn set(&mut self,index: Index, new_value: bool ) {
+        if new_value == true{
+            self.value = self.value | (1 << index.get());
+        }else{
+            self.value = self.value & !(1 << index.get());
+        }
     }
 }
 
 fn main() {
-    let mut bol_example = BitWise::new(1.into()).unwrap();
-    println!("{:08b}", bol_example.value);
-    bol_example.clear(1.into());
-    println!("{:08b}", bol_example.value);
-    let bit = BitWise {value:1};
-    for i in 0..=7{
-        println!(" index {i} = {}", bit.read(i.into()).unwrap());
-    }
+    // let mut bol_example = BitWise::new(1.into()).unwrap();
+    // println!("{:08b}", bol_example.value);
+    // bol_example.clear(1.into());
+    // println!("{:08b}", bol_example.value);
+    // let mut bit = BitWise {value:1};
+    // for i in 0..=7{
+    //     println!(" index {i} = {}", bit.read(i.into()).unwrap());
+    // }
+
+    // bit.set(1.into(), false);
+    //     println!();
+
+    // for i in 0..=7{
+    //     println!(" index {i} = {}", bit.read(i.into()).unwrap());
+    // }
 }
