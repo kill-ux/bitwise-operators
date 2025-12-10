f184d3228fe6e6ae72016bb044c8dcb35d1a9376
killux <mustaphaboutoubdev@gmail.com>
2025-12-10 14:58:08 +0100

    feture of clear

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 // READ | CLEAR | SET | TOGGLE
-struct BitWise(u8);
+struct BitWise {
+    pub value: u8,
+}
 
 impl BitWise {
+    pub fn new(index: u8) -> Result<Self, String> {
+        match index {
+            0..8 => Ok(BitWise { value: 3 }),
+            _ => Err("Choose an index between 0 and 7".to_string()),
+        }
+    }
+
+    pub fn clear(&mut self, index: u8) {
+        let mask: u8 = !(1u8 << index);
+        self.value &= mask;
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut bol_example = BitWise::new(1).unwrap();
+    println!("{:08b}", bol_example.value);
+    bol_example.clear(1);
+    println!("{:08b}", bol_example.value);
 }
