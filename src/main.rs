12ed2484cd8f19418acd1b3ccf28cd6e68c1a745
killux <mustaphaboutoubdev@gmail.com>
2025-12-10 15:28:36 +0100

    fix new os BitWise

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ use bitwise_operators::Index;
 
 // READ | CLEAR | SET | TOGGLE
 #[derive(Clone, Copy)]
-struct BitWise{
+struct BitWise {
     pub value: u8,
 }
 
 impl BitWise {
     pub fn new(index: Index) -> Result<Self, String> {
-        Ok(BitWise{value:1})
+        Ok(BitWise {
+            value: 1 << index.get(),
+        })
     }
 
     pub fn clear(&mut self, index: Index) {
         let mask: u8 = !(1u8 << index.get());
         self.value &= mask;
     }
-  
-    pub fn read(self,index: Index) -> Result<bool, String> {
+
+    pub fn read(self, index: Index) -> Result<bool, String> {
         Ok(self.value >> index.get() & 1 == 1)
     }
 }
@@ -26,8 +28,8 @@ fn main() {
     println!("{:08b}", bol_example.value);
     bol_example.clear(1.into());
     println!("{:08b}", bol_example.value);
-    let bit = BitWise {value:1};
-    for i in 0..=7{
+    let bit = BitWise { value: 1 };
+    for i in 0..=7 {
         println!(" index {i} = {}", bit.read(i.into()).unwrap());
     }
 }
